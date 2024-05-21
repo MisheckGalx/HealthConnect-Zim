d2a1b02b3c266caef8e4810bb033ade31798c28b
root <root@MisheckG>
2024-05-22 00:23:18 +0200

    service.js component to render the services section

---
+++ src/pages/service.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <div>
+            <h2>Our Services</h2>
+            <ul>
+                <li>General health</li>
+                <li>Mental health</li>
+                <li>Sexual health</li>
+                <li>Women's health</li>
+                <li>Men's health</li>
+                <li>Children's health</li>
+                <li>Chronic conditions</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Service;
