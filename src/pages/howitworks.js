71d1544cb3a8764624215da4d156e5e5137ee412
root <root@MisheckG>
2024-05-22 00:32:12 +0200

    howitworks.js component detailing the steps for using the app

---
+++ src/pages/howitworks.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HowItWorks = () => {
+    return (
+        <div>
+            <h2>How It Works</h2>
+            <ol>
+                <li>Download the HealthConnectZim app</li>
+                <li>Register with your mobile number and valid license or ID</li>
+                <li>Select the consultation type and category</li>
+                <li>Start your consultation</li>
+            </ol>
+        </div>
+    );
+};
+
+export default HowItWorks;
