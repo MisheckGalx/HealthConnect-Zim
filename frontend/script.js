2315d7de603dc228268c56f5e6c8df08b17585aa
root <root@MisheckG>
2024-05-21 14:35:32 +0200

    commits mades to the scripts

---
+++ frontend/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() => {
+  // Eaxample of adding dynamic content to the page
+  const appDiv = document.getElementById('app');
+  appDiv.innerHTML = '<h2>Makadini/Unjani welcome to HealthConnect Zim!</h2><p>Your gateway to quality healthcare services.</p>';
+})
