567226b7c77c4a33c3352aa7b448bea3e9922e30
root <root@MisheckG>
2024-05-22 00:07:46 +0200

    files for the consultation file

---
+++ src/components/consultation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Consultation = () => {
+    return (
+        <section>
+            <h2>Consult with a Nurse, Doctor or Therapist</h2>
+            <p>Get an online consultation with our professional medical team.</p>
+        </section>
+    );
+};
+
+export default Consultation;
